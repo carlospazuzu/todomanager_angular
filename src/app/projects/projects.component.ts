87bca9fdc938c7e1d1fb7936850b2e4b860473d5
Carlos Pazuzu <carlosh555@hotmail.com>
2020-01-08 16:47:10 -0300

    exibir labels dos projetos concluida

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,17 +10,25 @@ import { Router } from '@angular/router';
 export class ProjectsComponent implements OnInit {
 
   projects: [] = null;
+  // labelNamesArray: string[] = [];
+  labelNamesDict = {};
 
   constructor(private http: HttpClient, private router: Router) {
     
     let id = parseInt(sessionStorage.getItem('owner_id'));
 
     this.http.get('http://localhost:8000/projects/?owner=' + id).subscribe(data => {
-        this.projects = data['results'];
-        
+        this.projects = data['results'];   
+        for (let p of this.projects) {
+          let name: string = p['label'];
+          this.http.get(p['label']).subscribe(data => {
+            this.labelNamesDict[name] = data['name'];
+          });
+        }
       }      
-    );
-    
+    );    
+
+    console.log(this.labelNamesDict);
    }
 
   ngOnInit() {
@@ -28,6 +36,18 @@ export class ProjectsComponent implements OnInit {
 
   openActivitiesPage(projectId: string) {
     sessionStorage.setItem('project_id', projectId);
+  }
+
+  getPORRA(labelUrl) {
+    return this.labelNamesDict[labelUrl];
+  }
+
+  getLabelName(labelUrl) {
+    this.http.get(labelUrl)
+                  .subscribe(res => {
+                    this.labelNamesDict[labelUrl[labelUrl.length - 1]] = res['name'];
+                  });
+
   }
 
 }
